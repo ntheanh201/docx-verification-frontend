984f5b46db8d7c58c4ce6d162f0fbcdbb3c05072
ntheanh201 <ntheanh201@gmail.com>
2020-11-14 02:37:16 +0700

    error page

--- src/app/Auth/ErrorPage/NotFound.tsx
+++ src/app/Auth/ErrorPage/NotFound.tsx
@@ -1,5 +1,20 @@
 import { React } from 'core'
 
+import { Button, Result } from 'antd'
+import { useHistory } from 'router'
+
 export const NotFoundPage = () => {
-  return <div>404</div>
+  const history = useHistory()
+  return (
+    <Result
+      status='404'
+      title='404'
+      subTitle='Sorry, the page you visited does not exist.'
+      extra={
+        <Button type='primary' onClick={() => history.push('/')}>
+          Back Home
+        </Button>
+      }
+    />
+  )
 }

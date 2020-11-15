dcaf4be19e67ae2ce958dcae70bfe1c54702f5c7
The Anh <ntheanh201@gmail.com>
2020-11-15 13:57:43 +0700

    change backend server

--- src/environments/development.ts
+++ src/environments/development.ts
@@ -1,5 +1,5 @@
 //@ts-ignore
 module.exports = {
-  // serverUrl: 'https://dashboard.hirosume.tk/'
-  serverUrl: 'http://localhost:3001/'
+  serverUrl: 'https://dashboard.hirosume.tk/'
+  // serverUrl: 'http://localhost:3001/'
 }

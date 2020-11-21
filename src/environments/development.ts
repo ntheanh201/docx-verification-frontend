bbf0d3c6bde22f9154e094ef830d0f507344dbee
The Anh <ntheanh201@gmail.com>
2020-11-21 16:56:12 +0700

    change server url

--- src/environments/development.ts
+++ src/environments/development.ts
@@ -1,5 +1,5 @@
 //@ts-ignore
 module.exports = {
-  serverUrl: 'https://dashboard.hirosume.tk/'
-  // serverUrl: 'http://103.74.122.136:3001/'
+  // serverUrl: 'https://dashboard.hirosume.tk/'
+  serverUrl: 'http://103.74.122.136:3001/'
 }

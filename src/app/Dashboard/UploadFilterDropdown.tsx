c1506978881968485bfad1b7404a0a05474c34f1
The Anh <ntheanh201@gmail.com>
2020-11-25 02:08:53 +0700

    update ui

--- src/app/Dashboard/UploadFilterDropdown.tsx
+++ src/app/Dashboard/UploadFilterDropdown.tsx
@@ -1,30 +1,28 @@
-import React from 'react'
-import { Button, Input, Space } from 'antd'
+import { useCallback, useRef, useState, React, styled } from 'core'
+import { Button, Input, Space, Checkbox } from 'antd'
 import { SearchOutlined } from '@ant-design/icons'
-import { useCallback, useRef, useState } from 'core'
 import { userService } from 'service'
 
 const UploaderFilterDropdown = ({
-                                  setSelectedKeys,
-                                  selectedKeys,
-                                  confirm,
-                                  clearFilters
-                                }) => {
+  setSelectedKeys,
+  selectedKeys,
+  confirm,
+  clearFilters
+}) => {
   const ref = useRef()
   const [value, setValue] = useState('')
   const [list, setList] = useState([])
 
-  const onChange = useCallback((e) => {
+  const onChange = useCallback(e => {
     setValue(e.target.value)
   }, [])
-  const handleSearch = useCallback((value) => {
-    userService.search(value).then(res => [
-      setList(res)
-    ])
+  const handleSearch = useCallback(value => {
+    userService.search(value).then(res => [setList(res)])
   }, [])
   const handleReset = useCallback(clearFilters => {
     clearFilters()
   }, [])
+
   return (
     <div style={{ padding: 8 }}>
       <Input
@@ -35,9 +33,15 @@ const UploaderFilterDropdown = ({
         onPressEnter={() => handleSearch(value)}
         style={{ width: 188, marginBottom: 8, display: 'block' }}
       />
-      {
-        list.map(user => user.name)
-      }
+      {/* {list.map(user => user.name)} */}
+      {list.map(({ name }, index) => (
+        <Wrapper>
+          <Checkbox key={index} onChange={onChange}>
+            {name}
+          </Checkbox>
+        </Wrapper>
+      ))}
+
       <Space>
         <Button
           type='primary'
@@ -61,3 +65,7 @@ const UploaderFilterDropdown = ({
 }
 
 export default UploaderFilterDropdown
+
+const Wrapper = styled.div`
+  margin-bottom: 8px;
+`
